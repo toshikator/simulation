b8f83eca256efe5d55ed8b16b1443920a62c1106
AntonBukhman <diamondspb2015@gmail.com>
2024-11-06 00:51:44 +0200

    OOP structure was modified. Simulation class was separated.

--- src/main/java/second_project/simulation/SimulationApplication.java
+++ src/main/java/second_project/simulation/SimulationApplication.java
@@ -11,30 +11,17 @@ import second_project.simulation.entities.resources.Rock;
 import second_project.simulation.entities.resources.Tree;
 import second_project.simulation.map.Map;
 import second_project.simulation.renderer.ConsoleRenderer;
+import second_project.simulation.simulation.Simulation;
 
 import java.util.HashMap;
 
 public class SimulationApplication {
 
     public static void main(String[] args) {
+
         Coordinates worldSize = new Coordinates(AppConstants.worldHeight, AppConstants.worldWidth);
         Map map = new Map(worldSize);
-        HashMap<Entity, Integer> resources = new HashMap<>();
-        ConsoleRenderer consoleRenderer = ConsoleRenderer.getInstance();
-        HashMap<Entity, Integer> creatures = new HashMap<>();
-        ResourcesDistributor resourcesDistributor = new ResourcesDistributor(map, resources);
-        CreaturesDistributor creaturesDistributor = new CreaturesDistributor(map, creatures);
-
-        resources.put(new Grass(), AppConstants.grassAmount);
-        resources.put(new Rock(), AppConstants.rocksAmount);
-        resources.put(new Tree(), AppConstants.treesAmount);
-        creatures.put(new Carnivore(map), AppConstants.carnivoreAmount);
-        creatures.put(new Herbivore(map), AppConstants.herbivoreAmount);
-        resourcesDistributor.applyStartPositions();
-        creaturesDistributor.applyStartPositions();
-
-        while (true) {
-            consoleRenderer.render(map);
-        }
+        Simulation simulation = new Simulation(map);
+        simulation.renderMap();
     }
 }
--- src/main/java/second_project/simulation/simulation/Simulation.java
+++ src/main/java/second_project/simulation/simulation/Simulation.java
@@ -1,4 +1,44 @@
 package second_project.simulation.simulation;
 
+import second_project.simulation.AppConstants;
+import second_project.simulation.Coordinates;
+import second_project.simulation.actions.initActions.CreaturesDistributor;
+import second_project.simulation.actions.initActions.ResourcesDistributor;
+import second_project.simulation.entities.Entity;
+import second_project.simulation.entities.creatures.Carnivore;
+import second_project.simulation.entities.creatures.Herbivore;
+import second_project.simulation.entities.resources.Grass;
+import second_project.simulation.entities.resources.Rock;
+import second_project.simulation.entities.resources.Tree;
+import second_project.simulation.map.Map;
+import second_project.simulation.renderer.ConsoleRenderer;
+
+import java.util.HashMap;
+
 public class Simulation {
+    private final Map map;
+    private final ConsoleRenderer consoleRenderer;
+    private final HashMap<Entity, Integer> resources;
+    HashMap<Entity, Integer> creatures;
+
+    public Simulation(Map map) {
+        this.map = map;
+        resources = new HashMap<>();
+        consoleRenderer = ConsoleRenderer.getInstance();
+        creatures = new HashMap<>();
+        ResourcesDistributor resourcesDistributor = new ResourcesDistributor(map, resources);
+        CreaturesDistributor creaturesDistributor = new CreaturesDistributor(map, creatures);
+
+        resources.put(new Grass(), AppConstants.grassAmount);
+        resources.put(new Rock(), AppConstants.rocksAmount);
+        resources.put(new Tree(), AppConstants.treesAmount);
+        creatures.put(new Carnivore(map), AppConstants.carnivoreAmount);
+        creatures.put(new Herbivore(map), AppConstants.herbivoreAmount);
+        resourcesDistributor.applyStartPositions();
+        creaturesDistributor.applyStartPositions();
+    }
+
+    public void renderMap() {
+        consoleRenderer.render(map);
+    }
 }
