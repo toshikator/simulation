90a2c170bc7da24acc9e31ae937cb931d166284e
AntonBukhman <diamondspb2015@gmail.com>
2025-01-07 22:41:04 +0200

    Menu added Possibility to change game constants added

--- src/main/java/second_project/simulation/SimulationApplication.java
+++ src/main/java/second_project/simulation/SimulationApplication.java
@@ -9,15 +9,29 @@ public class SimulationApplication {
 
     public static void main(String[] args) {
 
-        SimulationMap simulationMap = SimulationMap.getInstance();
-//        MapUtility.setMap(simulationMap);
-        Simulation simulation = new Simulation();
-        simulation.renderMap();
+        char reply = 0;
+        System.out.println("Welcome to the simulation!");
+        while (reply != 'e') {
+            reply = 0;
+            System.out.println("Would you like to start the simulation?");
+            System.out.println("Type 'y' to start the simulation");
+            System.out.println("Type 'e' for exit");
+            System.out.println("Type 's' to change the simulation settings");
+            reply = MenuUtility.getCharacter();
+            if (reply == 'y') {
+                SimulationMap simulationMap = SimulationMap.getInstance();
+                Simulation simulation = new Simulation();
+                simulation.renderMap();
 
-        for (int i = 0; i < GAME_TURNS; i++) {
-            System.out.println();
-            simulation.makeMove();
-            simulation.renderMap();
+                for (int i = 0; i < GAME_TURNS; i++) {
+                    System.out.println();
+                    simulation.makeMove();
+                    simulation.renderMap();
+                }
+            }
+            if (reply == 's') {
+                MenuUtility.changeSettings();
+            }
         }
     }
 }
